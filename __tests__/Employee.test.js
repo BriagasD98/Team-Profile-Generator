45252a2c4f638eb1f40271d0ebce390a33ddf7b0
BriagasD98 <briagasdavid@yahoo.com>
2021-07-17 16:24:21 -0500

    Employee tests created and passed, expect.js added

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+const { expectToBe, expectStr, expectNum } = require('../utils/expect');
+
+const employee = new Employee('Han', 'Solo', 1);
+
+test('creates an employee object', () => {
+    expectToBe(employee.firstName, 'Han');
+    expectToBe(employee.lastName, 'Solo');
+    expectNum(employee.id);
+    expectStr(employee.email, '@');
+    expectToBe(employee.role, 'Employee');
+    expectToBe(employee.icon, 'fas fa-briefcase');
+});
+
+test('gets employee\'s name', () => {
+    expectStr(employee.getName(), `${employee.firstName} ${employee.lastName}`);
+});
+
+test('gets employee\'s ID', () => {
+    expectStr(employee.getId(), `${employee.id}`);
+});
+
+test('gets employee\'s email', () => {
+    expectStr(employee.getEmail(), employee.email);
+});
+
+test('gets employee\'s role', () => {
+    expectStr(employee.getRole(), employee.role);
+});
+
+test('gets employee\'s icon', () => {
+    expectStr(employee.getIcon(), employee.icon);
+});
